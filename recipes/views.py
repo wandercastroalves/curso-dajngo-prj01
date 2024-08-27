d622c8accf9ce309c2a9eceaa35032a036be2580
Wander Castro <wander@micronetbrasil.com>
2024-08-26 23:15:00 -0300

    alt .urls

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+#from django.shortcuts import render
+
+
+def home(request):
+    return HttpResponse('Home new')
+
+def contato(request):
+    return HttpResponse('Contato new')
+
+
+def sobre(request):
+    return HttpResponse('Abaout new ')
