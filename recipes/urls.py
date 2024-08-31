ada31c08b5f135bdce1f0d7ff3bdadcdc9244d19
Wander Castro <wander@micronetbrasil.com>
2024-08-31 20:33:04 -0300

    Removendo testes

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from recipes.views import contato, home, sobre
+from recipes.views import home
 
 urlpatterns = [
     path('',home),
-    path('contato/',contato),
-    path('sobre/',sobre),
+   
 ]
